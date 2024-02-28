54210ad81ee050285e2c0444f57530649179fcbb
Lemondemones <sergej.omelchenko.02@mail.ru>
2024-02-28 22:57:42 +0300

    fix mistakes

--- utils/watcher.js
+++ utils/watcher.js
@@ -1,50 +1,49 @@
 import "dotenv/config";
 import chokidar from "chokidar";
 import { sendEmailNotification } from "../service/mailService.js";
-import { manageUserFile } from "./manageFile.js";
 import path from "path";
 
 let fileAddList = {}; // Объект для хранения массивов файлов для каждого пользователя
 let timer = {}; // Объект для хранения таймеров для каждого пользователя
 
 export function runFileWatcher(dir) {
-  const watcher = chokidar.watch(dir, {
-    ignored: [/(^|[\/\\])\../, /(^|[\/\\])appdata_ocbyr6147skr([\/\\]|$)/, "**/uploads/**", /test_imu/],
-    persistent: true,
-    ignoreInitial: true,
-    awaitWriteFinish: {
-      stabilityThreshold: 10000,
-      pollInterval: 100,
-    },
-  });
-
-  const log = console.log.bind(console);
-
-  watcher
-    .on("add", async (filePath) => {
-      const userDir = path.basename(path.dirname(filePath));
-
-      if (!fileAddList[userDir]) {
-        fileAddList[userDir] = [];
-      }
-
-      fileAddList[userDir].push(filePath);
-
-      if (timer[userDir]) {
-        clearTimeout(timer[userDir]);
-      }
-
-      timer[userDir] = setTimeout(() => {
-        sendEmailNotification(userDir, fileAddList[userDir]);
-        fileAddList[userDir] = [];
-        clearTimeout(timer[userDir]);
-        timer[userDir] = null;
-      }, 10000);
-
-      log(`New file detected: ${filePath}`);
-    })
-    .on("error", async (error) => {
-      log(`Watcher error: ${error}`);
-      await sendEmailNotification(error);
+    const watcher = chokidar.watch(dir, {
+        ignored: [/(^|[\/\\])\../, /(^|[\/\\])appdata_ocbyr6147skr([\/\\]|$)/, "**/uploads/**", /test_imu/],
+        persistent: true,
+        ignoreInitial: true,
+        awaitWriteFinish: {
+            stabilityThreshold: 10000,
+            pollInterval: 100,
+        },
     });
+
+    const log = console.log.bind(console);
+
+    watcher
+        .on("add", async (filePath) => {
+            const userDir = path.basename(path.dirname(filePath));
+
+            if (!fileAddList[userDir]) {
+                fileAddList[userDir] = [];
+            }
+
+            fileAddList[userDir].push(filePath);
+
+            if (timer[userDir]) {
+                clearTimeout(timer[userDir]);
+            }
+
+            timer[userDir] = setTimeout(() => {
+                sendEmailNotification(userDir, fileAddList[userDir]);
+                fileAddList[userDir] = [];
+                clearTimeout(timer[userDir]);
+                timer[userDir] = null;
+            }, 10000);
+
+            log(`New file detected: ${filePath}`);
+        })
+        .on("error", async (error) => {
+            log(`Watcher error: ${error}`);
+            await sendEmailNotification(error);
+        });
 }
